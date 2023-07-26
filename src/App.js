6181bf2a68e01827d2ec30c917bee1e4852ad4e5
jeliaaa <julia.aleqsandre@gmail.com>
2023-07-26 18:52:26 +0400

    added routing and edited some css, switched to sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainPage from "./Components/MainPage/MainPage";
 import Navbar from "./Components/Navbar/Navbar";
-
+import AboutUs from "./Components/AboutUs/AboutUs";
+import Booking from "./Components/Booking/Booking";
+import Services from "./Components/Services/Services"
+import ContactUs from "./Components/ContactUs/ContactUs"
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <MainPage />
+      <Routes>
+        <Route index path="/" element={<MainPage />} />
+        <Route path="/booking" element={<Booking />}/>
+        <Route path="/about" element={<AboutUs />}/>
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+
     </div>
   );
 }
